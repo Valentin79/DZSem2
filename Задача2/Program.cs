0f01434139a0d694771c4bc3059c6bd9dd2ae3c1
Valentin <zvezdochet.79@mail.ru>
2022-02-28 15:18:03 +0300

    Задача2

---
+++ Задача2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трёхзначное число 
+//и удаляет вторую цифру этого числа.
+
+int[] array = new int[3];
+int i = 0;
+while(i < 3)
+{
+    array[i] = new Random().Next(0, 9);
+    i++;
+}
+
+Console.WriteLine($"{array[0]}{array[1]}{array[2]} --> {array[0]}{array[2]}");
