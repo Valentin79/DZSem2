0e97f1a6185503e828eee444e063082930aec814
Valentin <zvezdochet.79@mail.ru>
2022-02-28 14:47:13 +0300

    Задача1

---
+++ Задача1alt/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите число: ");
+string a = Console.ReadLine();
+string str = a.ToString();
+int[] array = new int[str.Length];
+int i = 0;
+while(i < str.Length)
+{
+    array[i] = int.Parse(str[i].ToString());
+    i++;
+}
+Console.WriteLine(array[1]);
